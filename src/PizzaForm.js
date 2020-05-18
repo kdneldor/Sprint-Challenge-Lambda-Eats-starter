2bca4d6743b002033a1bee16ba6386cdd9f9c48f
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-05-17 20:18:20 -0500

    Attempted to add functionality, but cannot navigate between pages as a result

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -1,25 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-function PizzaForm() {
-  //     const [formState, setFormState] = useState(
-  //       name: "",
-  //       size: "",
-  // toppings: "",
+import * as yup from "yup";
 
-  //   );
+const formSchema = yup.object().shape({
+  name: yup.string().required("Name is a required field"),
+});
+
+function PizzaForm(props) {
+  const [formState, setFormState] = useState({
+    name: "",
+  });
+  const [errorState, setErrorState] = useState({
+    name: "",
+  });
+  const validate = (e) => {
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(e.target.value)
+      .then((valid) => {
+        setErrorState({
+          ...errorState,
+          [e.target.name]: "",
+        });
+      })
+      .catch((err) => {
+        console.log(err.errors);
+        setErrorState({
+          ...errorState,
+          [e.target.name]: err.errors[0],
+        });
+      });
+  };
+  const inputChange = (e) => {
+    e.persist();
+    validate(e);
+    let value =
+      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+    setFormState({ ...formState, [e.target.name]: value });
+  };
+  const formSubmit = (e) => {
+    // e.preventDefault();
+    console.log("Form Submitted!");
+    // axios
+    //   .post("https://reqres.in/api/users", formState)
+    //   .then((response) => {
+    //     console.log(props.addUser);
+    //     props.addUser(response.data);
+    //   })
+    //   .catch((err) => console.log(err));
+  };
 
   return (
-    <form className="pizza-form">
+    <form className="pizza-form" >
       <div className="home-button">
         <Link to="/">
           <button>Home</button>
         </Link>
       </div>
 
-      <label className="name-form" htmlFor="name">
+      <label className="name" htmlFor="name">
         Name:
         <br></br>
-        <input type="text" name="name" id="name" placeholder="Name"></input>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Name"
+          value={formState.name}
+        //   onChange={inputChange}
+        ></input>
       </label>
       <br></br>
       <br></br>
@@ -94,12 +143,12 @@ function PizzaForm() {
       </label>
       <br></br>
       <br></br>
-      <label htmlFor="motivation">
+      <label htmlFor="special-instructions">
         Special Instructions?
         <br></br>
         <textarea
-          name="motivation"
-          id="motivation"
+          name="special-instructions"
+          id="special-instructions"
           //   value={formState.motivation}
           //   onChange={inputChange}
         />
@@ -108,7 +157,7 @@ function PizzaForm() {
         ) : null} */}
       </label>
       <br></br>
-      <button>Add to Order</button>
+      <button>Add to Order!</button>
     </form>
   );
 }
